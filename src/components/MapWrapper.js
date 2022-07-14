5c589c5f9dc3555744748c666281e3d4bbe6a2d2
Erick Paiva <123skatesk8@gmail.com>
2022-07-13 21:13:31 -0400

    feat: I create the logic to render the marker in the map

--- src/components/MapWrapper.js
+++ src/components/MapWrapper.js
@@ -1,127 +1,105 @@
-// react
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
+import "ol/ol.css";
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import XYZ from "ol/source/XYZ";
+import { toLonLat, transform } from "ol/proj";
+import { toStringHDMS, toStringXY } from "ol/coordinate";
+import { Overlay } from "ol";
 
-// openlayers
-import Map from 'ol/Map'
-import View from 'ol/View'
-import TileLayer from 'ol/layer/Tile'
-import VectorLayer from 'ol/layer/Vector'
-import VectorSource from 'ol/source/Vector'
-import XYZ from 'ol/source/XYZ'
-import {transform} from 'ol/proj'
-import {toStringXY} from 'ol/coordinate';
+function MapWrapper({ features, callback }) {
+  const [map, setMap] = useState();
+  const [featuresLayer, setFeaturesLayer] = useState();
+  const [selectedCoord, setSelectedCoord] = useState();
 
-function MapWrapper(props) {
+  const mapElement = useRef();
 
-  // set intial state
-  const [ map, setMap ] = useState()
-  const [ featuresLayer, setFeaturesLayer ] = useState()
-  const [ selectedCoord , setSelectedCoord ] = useState()
+  const mapRef = useRef();
 
-  // pull refs
-  const mapElement = useRef()
-  
-  // create state ref that can be accessed in OpenLayers onclick callback function
-  //  https://stackoverflow.com/a/60643670
-  const mapRef = useRef()
-  mapRef.current = map
+  mapRef.current = map;
 
-  // initialize map on first render - logic formerly put into componentDidMount
-  useEffect( () => {
+  const overlay = new Overlay({
+    id: "info",
+    autoPan: {
+      animation: {
+        duration: 250,
+      },
+    },
+  });
 
-    // create and add vector source layer
+  useEffect(() => {
     const initalFeaturesLayer = new VectorLayer({
-      source: new VectorSource()
-    })
+      source: new VectorSource(),
+    });
 
-    // create map
     const initialMap = new Map({
       target: mapElement.current,
       layers: [
-        
-        // USGS Topo
         new TileLayer({
           source: new XYZ({
-            url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}',
-          })
+            url: "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}",
+          }),
         }),
-
-        // Google Maps Terrain
-        /* new TileLayer({
-          source: new XYZ({
-            url: 'http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}',
-          })
-        }), */
-
-        initalFeaturesLayer
-        
       ],
       view: new View({
-        projection: 'EPSG:3857',
+        projection: "EPSG:3857",
         center: [0, 0],
-        zoom: 2
+        zoom: 2,
       }),
-      controls: []
-    })
+      controls: [],
+      overlays: [overlay],
+    });
 
-    // set map onclick handler
-    initialMap.on('click', handleMapClick)
+    initialMap.on("singleclick", handleMapClick);
 
-    // save map and vector layer references to state
-    setMap(initialMap)
-    setFeaturesLayer(initalFeaturesLayer)
+    setMap(initialMap);
+    setFeaturesLayer(initalFeaturesLayer);
+  }, []);
 
-  },[])
-
-  // update map if features prop changes - logic formerly put into componentDidUpdate
-  useEffect( () => {
-
-    if (props.features.length) { // may be null on first render
-
-      // set features to map
+  useEffect(() => {
+    if (features.length) {
       featuresLayer.setSource(
         new VectorSource({
-          features: props.features // make sure features is an array
+          features: features,
         })
-      )
+      );
 
-      // fit map to feature extent (with 100px of padding)
       map.getView().fit(featuresLayer.getSource().getExtent(), {
-        padding: [100,100,100,100]
-      })
-
+        padding: [100, 100, 100, 100],
+      });
     }
+  }, [features]);
 
-  },[props.features])
-
-  // map click handler
   const handleMapClick = (event) => {
-    
-    // get clicked coordinate using mapRef to access current React state inside OpenLayers callback
-    //  https://stackoverflow.com/a/60643670
     const clickedCoord = mapRef.current.getCoordinateFromPixel(event.pixel);
 
-    // transform coord to EPSG 4326 standard Lat Long
-    const transormedCoord = transform(clickedCoord, 'EPSG:3857', 'EPSG:4326')
+    const transormedCoord = transform(clickedCoord, "EPSG:3857", "EPSG:4326");
 
-    // set React state
-    setSelectedCoord( transormedCoord )
-    
-  }
+    setSelectedCoord(transormedCoord);
 
-  // render component
-  return (      
+    const coordinate = event.coordinate;
+    const hdms = toStringHDMS(toLonLat(coordinate));
+
+    callback(hdms);
+    overlay.setPosition(coordinate);
+  };
+
+  useEffect(() => {
+    overlay.setElement(document.getElementById("popup"));
+  }, []);
+
+  return (
     <div>
-      
       <div ref={mapElement} className="map-container"></div>
-      
+
       <div className="clicked-coord-label">
-        <p>{ (selectedCoord) ? toStringXY(selectedCoord, 5) : '' }</p>
+        <p>{selectedCoord ? toStringXY(selectedCoord, 5) : ""}</p>
       </div>
-
     </div>
-  ) 
-
+  );
 }
 
-export default MapWrapper
+export default MapWrapper;
